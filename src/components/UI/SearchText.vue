3b09606dca63f8758222964dfb81d143e9f0aaea
Carlos Mendes <52838023+CarlosMendesDev@users.noreply.github.com>
2021-11-17 17:58:31 -0300

    Initialize google input

--- src/components/UI/SearchText.vue
+++ src/components/UI/SearchText.vue
@@ -1,5 +1,37 @@
 <template lang="pug">
-q-input(
-  label="Buscar Cidade"
-)
+input(id="searchtxt" @input="fodase($event)" v-model="teste")
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      teste: '',
+    };
+  },
+
+  watch: {
+    teste() {
+      console.log(this.teste);
+    },
+  },
+
+  methods: {
+    fodase(value) {
+      console.log(value.target.value);
+    },
+  },
+
+  mounted() {
+    const input = document.getElementById('searchtxt');
+
+    const options = {
+      types: ['(cities)'],
+      bounds: new google.maps.LatLngBounds(new google.maps.LatLng(-23.5489, -46.6388)),
+    };
+
+    // eslint-disable-next-line no-new
+    new google.maps.places.Autocomplete(input, options);
+  },
+};
+</script>
